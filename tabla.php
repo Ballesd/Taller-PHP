eaf775dab34c98ff2cea55f77cb8869da390b983
Ballesd <ballesterosj500@gmail.com>
2022-06-03 16:03:21 -0500

    PE

--- tabla.php
+++ tabla.php
@@ -1,5 +1,8 @@
 <?php
 include("db.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -72,3 +75,6 @@ include("db.php"); ?>
         </div>
     </div>
 </body>
+<script src = "validaciones.js"></script>
+
+</html>
